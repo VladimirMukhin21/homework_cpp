27382a483199c99938b32fbd19c164226ed0846a
VladimirMukhin21 <102899387+VladimirMukhin21@users.noreply.github.com>
2025-09-19 16:17:14 +0000

    Удален лишний комментарий

--- task_02/src/getMax.cpp
+++ task_02/src/getMax.cpp
@@ -3,7 +3,6 @@
 int GetMax(int *arr, int size) {
     int max = *arr;
     for (int i = 1; i < size; ++i) {
-        // max = max > arr[i] ? arr[i]:max; // True:False
         if (arr[i]>max){
             max=arr[i];
         }
--- task_02/src/test.cpp
+++ task_02/src/test.cpp
@@ -3,6 +3,5 @@
 
 TEST(Test, Simple) {
   int arr[] = {1,2,3,4,5};
-  // int* arr = new int[5];
   ASSERT_EQ(GetMax(arr, 5), 5);
 }
