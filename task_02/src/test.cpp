f6841c8436c728e57de1fe0c367df67c9d567cd8
VladimirMukhin21 <102899387+VladimirMukhin21@users.noreply.github.com>
2025-09-19 20:13:37 +0000

    Добавлены тесты

--- task_02/src/test.cpp
+++ task_02/src/test.cpp
@@ -1,7 +1,17 @@
 #include <gtest/gtest.h>
 #include "getMax.hpp"
 
-TEST(Test, Simple) {
-  int arr[] = {1,2,3,4,5};
-  ASSERT_EQ(GetMax(arr, 5), 5);
+TEST(Test, Test1) {
+  int arr1[] = {1,2,3,4,5};
+  ASSERT_EQ(GetMax(arr1, sizeof(arr1)/4), 5);
+}
+
+TEST(Test, Test2) {
+  int arr2[] = {-1,-2,-3,-4,-5};
+  ASSERT_EQ(GetMax(arr2, sizeof(arr2)/4), -1);
+}
+
+TEST(Test, Test3) {
+  int arr3[] = {9,-3,5,8,-1};
+  ASSERT_EQ(GetMax(arr3, sizeof(arr3)/4), 9);
 }
